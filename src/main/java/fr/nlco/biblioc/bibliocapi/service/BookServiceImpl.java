de3a15ab20bd350161f934e260695d4db0f299fc
Nicolas <nicolas.lejeune@axa.fr>
2020-02-06 14:20:34 +0100

    refacto(): Adding au mapper for DTO

--- src/main/java/fr/nlco/biblioc/bibliocapi/service/BookServiceImpl.java
+++ src/main/java/fr/nlco/biblioc/bibliocapi/service/BookServiceImpl.java
@@ -1,21 +1,22 @@
 package fr.nlco.biblioc.bibliocapi.service;
 
 import fr.nlco.biblioc.bibliocapi.dto.BookStockDto;
+import fr.nlco.biblioc.bibliocapi.mapper.BookStockMapper;
 import fr.nlco.biblioc.bibliocapi.model.Book;
 import fr.nlco.biblioc.bibliocapi.repository.BookRepository;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Implémentation de l'interface BookService
  */
 @Service("BookService")
 public class BookServiceImpl implements BookService {
+
+    private BookStockMapper mapper = Mappers.getMapper(BookStockMapper.class);
 
     private final BookRepository _BookRepository;
 
@@ -32,14 +33,6 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<BookStockDto> getBooksStock() {
         List<Book> books = _BookRepository.findAll();
-        List<BookStockDto> bookStock = new ArrayList<>();
-        for (Book b : books
-        ) {
-            Integer nbCopy = b.getCopies().size();
-            Integer nbAvailable = b.getCopies().stream().filter(copy -> Objects.isNull(copy.getLoan())).collect(Collectors.toList()).size();
-            BookStockDto t = new BookStockDto(b.getTitle(), b.getAuthor(), b.getType(), nbCopy, nbAvailable);
-            bookStock.add(t);
-        }
-        return bookStock;
+        return mapper.booksToBookStockDtos(books);
     }
 }
