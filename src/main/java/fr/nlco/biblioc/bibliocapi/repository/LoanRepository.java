b4c6227abf2cc425403ca9ba134dd163faf9385c
Nicolas <nicolas.lejeune@axa.fr>
2020-02-13 23:44:29 +0100

    feat(): Create Loan

--- src/main/java/fr/nlco/biblioc/bibliocapi/repository/LoanRepository.java
+++ src/main/java/fr/nlco/biblioc/bibliocapi/repository/LoanRepository.java
@@ -1,16 +1,32 @@
 package fr.nlco.biblioc.bibliocapi.repository;
 
+import fr.nlco.biblioc.bibliocapi.model.Copy;
 import fr.nlco.biblioc.bibliocapi.model.Loan;
 import fr.nlco.biblioc.bibliocapi.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Interface pour la couche DAO de la classe model Loan
  */
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
+    /**
+     * Retourne la liste des prêts d'un membre
+     *
+     * @param member numéro du membre
+     * @return la liste des prêts
+     */
     List<Loan> findLoansByMember(Member member);
+
+    /**
+     * Retourne un prêt à partir de l'id de l'exemplaire s'il existe
+     *
+     * @param copy id de l'exemplaire
+     * @return le prêt
+     */
+    Optional<Loan> findByCopy(Copy copy);
 }
--- src/main/java/fr/nlco/biblioc/bibliocapi/service/LoanServiceImpl.java
+++ src/main/java/fr/nlco/biblioc/bibliocapi/service/LoanServiceImpl.java
@@ -1,10 +1,13 @@
 package fr.nlco.biblioc.bibliocapi.service;
 
+import fr.nlco.biblioc.bibliocapi.dto.LoanDto;
 import fr.nlco.biblioc.bibliocapi.dto.MemberLateLoansDto;
 import fr.nlco.biblioc.bibliocapi.dto.MemberLoansDto;
 import fr.nlco.biblioc.bibliocapi.mapper.LoansMapper;
+import fr.nlco.biblioc.bibliocapi.model.Copy;
 import fr.nlco.biblioc.bibliocapi.model.Loan;
 import fr.nlco.biblioc.bibliocapi.model.Member;
+import fr.nlco.biblioc.bibliocapi.repository.CopyRepository;
 import fr.nlco.biblioc.bibliocapi.repository.LoanRepository;
 import fr.nlco.biblioc.bibliocapi.repository.MemberRepository;
 import org.mapstruct.factory.Mappers;
@@ -12,10 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -26,12 +26,14 @@ public class LoanServiceImpl implements LoanService {
 
     private final LoanRepository _LoanRepository;
     private final MemberRepository _MemberRepository;
+    private final CopyRepository _CopyRepository;
     private LoansMapper mapper = Mappers.getMapper(LoansMapper.class);
 
     @Autowired
-    public LoanServiceImpl(LoanRepository loanRepository, MemberRepository memberRepository) {
+    public LoanServiceImpl(LoanRepository loanRepository, MemberRepository memberRepository, CopyRepository copyRepository) {
         this._LoanRepository = loanRepository;
         this._MemberRepository = memberRepository;
+        this._CopyRepository = copyRepository;
     }
 
     /**
@@ -79,6 +81,26 @@ public class LoanServiceImpl implements LoanService {
                 lateLoans.add(mapper.memberLateLoansToMemberLateLoansDto(m, memberLateLoans));
         }
         return lateLoans.stream().filter(m -> m.getLateLoanList().size() > 0).collect(Collectors.toList());
+    }
+
+    /**
+     * Methode pour créer un prêt
+     *
+     * @param loanToCreate prêt à valider
+     * @return le prêt si validé
+     */
+    @Override
+    public Loan createLoan(LoanDto loanToCreate) {
+        Copy copyChecked = _CopyRepository.findById(loanToCreate.getCopyId()).orElse(null);
+        Optional<Member> memberChecked = _MemberRepository.findByMemberNumber(loanToCreate.getMemberNumber());
+        if (copyChecked == null || !memberChecked.isPresent()) return null;
+        Optional<Loan> loanCheck = _LoanRepository.findByCopy(copyChecked);
+        if (loanCheck.isPresent()) return null;
+        Loan loan = new Loan();
+        loan.setCopy(copyChecked);
+        loan.setMember(memberChecked.get());
+        loan.setLoanDate(new Date());
+        return _LoanRepository.save(loan);
     }
 
     /**
