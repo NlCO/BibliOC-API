f4e413fe7cd4bd4d6b66ef9ba376376355514c70
Nicolas <nicolas.lejeune@axa.fr>
2020-02-21 00:14:47 +0100

    feat(): Add Swagger

--- src/main/java/fr/nlco/biblioc/bibliocapi/BibliocapiApplication.java
+++ src/main/java/fr/nlco/biblioc/bibliocapi/BibliocapiApplication.java
@@ -3,9 +3,11 @@ package fr.nlco.biblioc.bibliocapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableSwagger2
 public class BibliocapiApplication {
 
     public static void main(String[] args) {
